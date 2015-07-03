ea5dc9decbf3fd28be64f1580a6734803919a6b9
Paul Felt <paul.lewis.felt@gmail.com>
2015-07-03 10:35:27 -0600

    updated file path

--- plf1/TAACL-2015-Vector-submission/csv/sync.sh
+++ plf1/TAACL-2015-Vector-submission/csv/sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -Paz plf1@nlp.cs.byu.edu:~/git/Experiments/plf1/cslda-paper/csv/* .
+rsync -Paz plf1@nlp.cs.byu.edu:/net/perplexity/plf1/git/Experiments/plf1/TAACL-2015-Vector-submission/csv/* .
